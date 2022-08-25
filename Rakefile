d6f02f54e2ef3ad174024148ddaa9d0d7dbb5157
David <davidongaroh@gmail.com>
2022-08-25 14:01:37 +0300

    incomplete

--- Rakefile
+++ Rakefile
@@ -1,8 +1,24 @@
-require_relative './config/environment'
-require 'sinatra/activerecord/rake'
+# require_relative './config/environment'
+# require 'sinatra/activerecord/rake'
 
 # CREATE TASK WITHOUT DESCRIPTION
-
+task :hello do 
+    puts "Hello mate!"
+end
 # CREATE TASK WITH DESCRIPTION
-
+task :complex_math do 
+    output = (0.01/100*14000000)
+    puts output
+end
 # ORGANIZE TASKS IN NAMESPACES
+namespace:math_concepts do
+    desc "find addition"
+    task :add do
+        puts "addition"
+    end
+
+    desc "find subtraction"
+    task :sub do
+        puts "subtraction"
+    end
+end
--- app/app.rb
+++ app/app.rb
@@ -2,11 +2,24 @@ require "pry"
 require "active_record"
 
 # TODO: CONNECT TO THE DATABASE
-
+ActiveRecord::Base.establish_connection(
+    adapter:"sqlite3",
+    database:"db/school.db"
+)
 # TODO: CREATE TABLE
+create_courses_table = <<-SQL
+CREATE TABLE IF NOT EXISTS courses(
+    id INTEGER PRIMARY KEY,
+    name  TEXT,
+    description TEXT,
+    duration INTEGER
+)
+SQL
+ActiveRecord::Base.connection.execute(create_courses_table)
 
 # TODO: Create class that associates with the db table
-
+class Course < ActiveRecord::Base
+end
 # TODO: VIEW METHODS AVAILABLE FOR TABLE (methods)
 
 # TODO: COLUMN NAMES (column_names)
@@ -20,3 +33,6 @@ require "active_record"
 # TODO: FIND BY GIVEN CONDITIONS (find_by)
 
 # TODO: UPDATE RECORDS (save)
+
+
+binding.pry
